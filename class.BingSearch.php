10db28b9a33a019e6fdf205a3f27721a7c840f98
Ayesh Karunaratne <ayeshlakmal@gmail.com>
2013-05-06 14:53:57 +0530

    Added curl, file_get_contents, next(). unstable

--- class.BingSearch.php
+++ class.BingSearch.php
@@ -4,13 +4,23 @@ class BingSearch {
   public $base_url = 'https://api.datamarket.azure.com/Bing/Search/';
   public $skip = 0;
   public $query = '';
+  public $top = 50;
   protected $results = 50;
+  public $fetchers = array('curl', 'file_get_contents');
+  public $fetcher = 'file_get_contents';
   
   function __construct($search, $start = 0, $results = 50, $appid = NULL) {
     $this->query = $search;
     $this->skip = $start;
+    $this->top = $results;
     if (!is_null($appid)) {
       $this->appid = $appid;
+    }
+  }
+  
+  function set_fetcher($method) {
+    if (method_exists($this, $method) && in_array($method, $this->fetchers)) {
+      $this->fetcher = $method;
     }
   }
   
@@ -39,5 +49,52 @@ class BingSearch {
     }
   }
   
+  function build_context() {
+    
+    return $context;
+  }
+  
+  function next_page() {
+    $this->skip += $this->top;
+  }
+  
+  protected function fetch() {
+    $url = $this->build_url();
+    return $this->{$this->fetcher}($url, $this->appid);
+  }
+  
+  function search() {
+    print '<pre>';
+      print_r(json_decode($this->fetch()));
+    print '</pre>';
+  }
+  
+  protected function file_get_contents($url, $appid) {
+    $url = $this->build_url();
+    $context = stream_context_create(
+      array(
+        'http' => array(
+          'request_fulluri' => true,       
+          'header'  => "Authorization: Basic " . base64_encode($appid . ':' . $appid)
+        )
+      )
+    );
+    return file_get_contents($url, 0, $context);
+  }
+  
+  protected function curl($url, $appid) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    curl_setopt($ch, CURLOPT_USERPWD, "$appid:$appid");
+    curl_setopt($ch, CURLOPT_HEADER, FALSE);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    $data = curl_exec($ch);
+    curl_close($ch);
+    return $data;
+  }
+
   
 }
--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php 
  include 'class.BingSearch.php';
- $bing = new BingSearch('Ayesh');
+ $bing = new BingSearch('Ayesh Karunaratne', 5000);
+ $bing->set_fetcher('curl');
  $bing->debug_url();
+ //$bing->next_page();
+ $bing->debug_url();
+ $bing->search();
